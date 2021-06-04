e9f46479417a674b534b7617de834e63c09e476e
unknown <namelessxcoded@gmail.com>
2021-06-04 13:11:19 +0545

    Added products card, custom scroll bar

---
+++ data/products.js
@@ -0,0 +1,26 @@
+var productsList = [
+        {
+            "id":101,
+            "name": "Dell Laptop",
+            "img": "images/img1.png",
+            "model": "Latitude",
+            "colors":["White","Blue","Black"],
+            "catagory": "Business"
+        },
+        {
+            "id":102,
+            "name": "HP Laptop",
+            "img": "images/img2.png",
+            "model": "Pavilion",
+            "colors":["White","Blue","Black"],
+            "catagory":"Business"
+        },
+        {
+            "id":103,
+            "name": "Acer Laptop",
+            "img": "images/img3.png",
+            "model": "Nitro 5",
+            "colors":["Red","Blue","Black"],
+            "catagory": "Gaming"
+        }
+    ];
